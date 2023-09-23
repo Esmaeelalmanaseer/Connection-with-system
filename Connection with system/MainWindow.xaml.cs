a0cc0020f5e75923ab5df839a6a9f48debb49983
esmael almanaseer <esmael almanaseer@DESKTOP-544600A>
2023-09-23 17:41:59 +0300

    Add project files.

---
+++ Connection with system/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.Common;
+
+namespace Connection_with_system
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Repostortemployee employeerepostory = new Repostortemployee();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataGridview1.ItemsSource = employeerepostory.getAll();
+        }
+
+        private void ADbtn(object sender, RoutedEventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(Firtxt.Text) && !string.IsNullOrEmpty(Lasttxt.Text))
+            {
+                employeerepostory.ADD(new Employee() { FirstName = Firtxt.Text, LastName = Lasttxt.Text });
+                Firtxt.Text = string.Empty;
+                Lasttxt.Text = string.Empty;
+            }
+            DataGridview1.ItemsSource = employeerepostory.getAll();
+        }
+
+        private void SelcetItem(object sender, SelectionChangedEventArgs e)
+        {
+            if (DataGridview1.SelectedItems.Count > 0)
+            {
+                var row = DataGridview1.SelectedItems[0];
+                var emplyoee = (Employee)row;
+                LableId.Content = emplyoee.ID.ToString();
+                TXTF.Text = emplyoee.FirstName;
+                TXTL.Text = emplyoee.LastName;
+            }
+        }
+
+        private void UDbtn(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(TXTF.Text) && !string.IsNullOrEmpty(TXTL.Text))
+            {
+                employeerepostory.UPdate(new Employee()
+                {
+                    ID = int.Parse(LableId.Content.ToString().Trim()),
+                    FirstName = TXTF.Text.Trim(),
+                    LastName = TXTL.Text.Trim()
+                });
+                TXTF.Text = string.Empty;
+                TXTL.Text = string.Empty;
+                DataGridview1.ItemsSource = employeerepostory.getAll();
+            }
+           
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(LableId.Content.ToString())) ;
+            {
+                employeerepostory.Delete(int.Parse(LableId.Content.ToString()));
+                
+            }
+            TXTF.Text = string.Empty;
+            TXTL.Text = string.Empty;
+            DataGridview1.ItemsSource = employeerepostory.getAll();
+        }
+    }
+   
+}
---
+++ Connection with system/Repostortemployee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.Common;
+
+namespace Connection_with_system
+{
+    class Repostortemployee
+    {
+        DbProviderFactory factory;
+        string provider;
+        string connectionString;
+        public Repostortemployee()
+        {
+            provider = ConfigurationManager.AppSettings["provider"];
+            connectionString = ConfigurationManager.AppSettings["connectionString"];
+            factory = DbProviderFactories.GetFactory(provider);
+        }
+        public List<Employee>getAll()
+        {
+            //جمع معلومات على شكل ليست
+            var emplyees = new List<Employee>();
+            //اتصال من الفاكتوري
+            using (var conniction = factory.CreateConnection()) 
+            {
+                //معرفه سيرفر و اسم داتا بيس
+                conniction.ConnectionString = connectionString;
+                //خلق اتصال
+                conniction.Open();
+                var command = factory.CreateCommand();
+                command.Connection = conniction;
+                //الان ارسال الامر الى داتا بيس 
+                //ارسال امر بلغه sql
+                command.CommandText = "Select * From Employess";
+                //بيانات تم قرائتها و ارجاعها
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                    //لما يكون ريدر لها قابليه على قرائه
+                    while(reader.Read())
+                    {
+                        //تعريف انانيموس و تحويل تايب نفس داتا بيس استدعاء ريدر نفس اسم داتا بيس موجوده
+                        emplyees.Add(new Employee() { ID = (int)reader["id"], FirstName = (string)reader["FirstName"], LastName = (string)reader["LastName"] });
+                    }
+                }
+                
+
+            }
+            return emplyees;
+        }
+        public void ADD(Employee emplyee)
+        {
+            using(var conniction =factory.CreateConnection())
+            {
+                conniction.ConnectionString = connectionString;
+                conniction.Open();
+                var command = factory.CreateCommand();
+                command.Connection = conniction;
+                command.CommandText = $"Insert Into Employess(FirstName , LastName)Values('{emplyee.FirstName}','{emplyee.LastName}');";
+                command.ExecuteNonQuery();
+            }
+
+        }
+        public void UPdate(Employee employee)
+        {
+            using(var conniction =factory.CreateConnection())
+            {
+                conniction.ConnectionString = connectionString;
+                conniction.Open();
+                var command = factory.CreateCommand();
+                command.Connection = conniction;
+                command.CommandText = $"Update Employess Set FirstName = '{employee.FirstName}', LastName ='{employee.LastName}' WHERE Id ='{employee.ID}';";
+                command.ExecuteNonQuery();
+            }
+                    
+        }
+        public void Delete(int id)
+        {
+            using(var conniction=factory.CreateConnection())
+            {
+                conniction.ConnectionString = connectionString;
+                conniction.Open();
+                var command = factory.CreateCommand();
+                command.Connection = conniction;
+                command.CommandText = $"Delete From Employess Where Id = {id};";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
